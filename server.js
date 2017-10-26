a3ee7dfe716701d019e40460e29370d924c9dc4d
Yvanna Harris <yvannaharris@yvannas-air.attlocal.net>
2017-10-26 14:56:06 -0400

    Handlebars main

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ var bodyParser = require("body-parser");
 var logger = require("morgan");
 var mongoose = require("mongoose");
 
-var axios = require("axios");
-var cheerio = require("cheerio");
 var request = require("request");
+var cheerio = require("cheerio");
 
-//var db = require("./models");
+
+var db = require("./models");
 
 var PORT = 3000;
 
@@ -19,28 +19,52 @@ app.use(bodyParser.urlencoded({ extended: false}));
 
 app.use(express.static("public"));
 
-console.log("\n***********************************\n" +
-            "Grabbing articles\n" +
-            "from CNN Technology" +
-            "\n***********************************\n");
+var exphbs = require("express-handlebars");
 
-request("https://www.nytimes.com/section/technology?action=click&pgtype=Homepage&region=TopBar&module=HPMiniNav&contentCollection=Tech&WT.nav=page", function(error, response, html) {
-	var $ = cheerio.load(html);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-	var results = [];
-	$("div.story-body").each(function(i, element) {
-		var headline = $(element).find("h2.headline").text();
+mongoose.Promise = Promise;
 
-		var link = $(element).find("a").attr("href");
-
-		var summary = $(element).find("p.summary").text();
-
-		results.push({
-			headline: headline,
-			link: link,
-			summary: summary
-		});
-	});
-
-	console.log(results);
+mongoose.connect("mongodb://localhost/newsScraper", {
+	useMongoClient: true
 });
+
+
+app.get("/scrape", function(req, res) {
+	request("https://www.nytimes.com/section/technology?action=click&pgtype=Homepage&region=TopBar&module=HPMiniNav&contentCollection=Tech&WT.nav=page", function(error, response, html) {
+		var $ = cheerio.load(html);
+
+		$("div.story-body").each(function(i, element) {
+
+			var result = {};
+
+			result.headline = $(this).find("h2.headline").text();
+
+			result.link = $(this).find("a").attr("href");
+
+			result.summary = $(element).find("p.summary").text();
+
+			db.Article.create(result).then(function(dbArticle) {
+				res.send("Scrape Complete");
+			})
+			.catch(function(err) {
+				res.json(err);
+			});
+		});
+	}); 
+});
+
+app.get("/articles", function(req, res) {
+	db.Article.find({}).then(function(dbArticle) {
+		res.json(dbArticle);
+	})
+	.catch(function(err) {
+		res.json(err);
+	});
+});
+
+app.listen(PORT, function() {
+  console.log("App running on port " + PORT + "!");
+});
+
